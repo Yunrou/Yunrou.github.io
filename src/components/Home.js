782b0a067fddd3bd0a9ad681b8b345ca0201f650
Yunrou <tao86419@gmail.com>
2020-09-22 17:21:28 +0800

    modify about me and resume

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,40 +6,39 @@ export default class Home extends Component {
       <Fragment>
           <div className="header-container">
             <div className="header">
-            <img src="./static/img/rou04.jpg" className="profile-img"/>
-            
-            <div className="header-content">
-              <div className="title">ABOUT ME</div>
-              <div className="content">
-                <p>
-                Hi, I am Yun-Rou (Lilian) Lin, a computer science student, 
-                currently studying for the master degree at National Chiao Tung University in Taiwan.
-                </p>
-                
-                <p>
-                I am interested in Data Science--including Artificial Intelligence, Data Mining, Data Visualization, and so on and so forth.
-                I enjoy acquiring new knowledge, 
-                yet I still have a long way to go due to a wide variety of knowledge in computer science.
-                "Learning is a lifelong journey!" 
-                In addition, via participating in social engagements and cooperation projects in university,
-                I become an astute and open-minded team worker. 
-                </p>
-                
-                <p>
-                Furthermore, knowledge in aesthetics and psychology engages me.
-                Reading psychological ariticles and books makes me
-                become a more mature individual with robust mindset.
-                Appreciating artworks inspires and intrigues me to come up with creative ideas,
-                and be more sensitive to the surroundings.
-                Consequently, I am capable of conquering difficulties without fear
-                and develop innovative thoughts.
-                </p>
-                
-                <p>
-                I love challenges and the sense of accomplishment.
-                <l>Just take risks and go for it!</l>
-                </p>
-              </div>
+              <img src="./static/img/rou04.jpg" className="profile-img"/>
+              <div className="block">
+                <div className="title">ABOUT ME</div>
+                <div className="content">
+                  <p>
+                    Hi, I am Yun-Rou (Lilian) Lin, a computer science student, 
+                    currently studying for the master degree at National Chiao Tung University in Taiwan.
+                    </p>
+                    
+                    <p>
+                    I am interested in Data Science--including Artificial Intelligence, Data Mining, Data Visualization, and so on and so forth.
+                    I enjoy acquiring new knowledge, 
+                    yet I still have a long way to go due to a wide variety of knowledge in computer science.
+                    "Learning is a lifelong journey!" 
+                    In addition, via participating in social engagements and cooperation projects in university,
+                    I become an astute and open-minded team worker. 
+                    </p>
+                    
+                    <p>
+                    Furthermore, knowledge in aesthetics and psychology engages me.
+                    Reading psychological ariticles and books makes me
+                    become a more mature individual with robust mindset.
+                    Appreciating artworks inspires and intrigues me to come up with creative ideas,
+                    and be more sensitive to the surroundings.
+                    Consequently, I am capable of conquering difficulties without fear
+                    and develop innovative thoughts.
+                    </p>
+                    
+                    <p>
+                    I love challenges and the sense of accomplishment.
+                    <l>Just take risks and go for it!</l>
+                    </p>
+                </div>
               </div>
             </div>
           </div>
