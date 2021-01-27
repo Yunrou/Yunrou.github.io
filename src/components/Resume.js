6780438fe67a61b1d8ee95cdc83109eb64c4d45c
Yunrou <tao86419@gmail.com>
2021-01-27 16:25:02 +0800

    Update education information

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -11,19 +11,19 @@ export default class Resume extends Component {
                   <div className="subtitle">Master of Science</div>
                   <div className="content">
                     <p>
-                    2019-present<br>
-                    National Chiao Tung University - Taiwan<br>
-                    Dept. of Computer Science & Inst. of Multimedia Engineering<br>
-                    Academics: GPA 4.22/4.3
+                      2019-present<br/>
+                      National Chiao Tung University - Taiwan<br/>
+                      Dept. of Computer Science & Inst. of Multimedia Engineering<br/>
+                      Academics: GPA 4.22/4.3
                     </p>
                   </div>
                   <div className="subtitle">Bachelor of Science</div>
                   <div className="content">
                     <p>
-                    2015-2019<br>
-                    National Chiao Tung University - Taiwan<br>
-                    Dept. of Computer Science<br>
-                    Academics: GPA 3.9/4.3
+                      2015-2019<br/>
+                      National Chiao Tung University - Taiwan<br/>
+                      Dept. of Computer Science<br/>
+                      Academics: GPA 3.9/4.3
                     </p>
                   </div>
               </div>
