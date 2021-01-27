850af123ae6ebbf1fb405f01da78fc0247266309
Yunrou <tao86419@gmail.com>
2021-01-27 17:08:54 +0800

    Update experience information

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -12,7 +12,7 @@ export default class Resume extends Component {
                   <div className="subtitle">Master of Science</div>
                   <div className="content">
                     <p>
-                      (2019-present)<br/>
+                      (2019 - present)<br/>
                       National Chiao Tung University - Taiwan<br/>
                       Dept. of Computer Science & Inst. of Multimedia Engineering<br/>
                       Academics: GPA 4.22/4.3
@@ -23,7 +23,7 @@ export default class Resume extends Component {
                   <div className="subtitle">Bachelor of Science</div>
                   <div className="content">
                     <p>
-                      (2015-2019)<br/>
+                      (2015 - 2019)<br/>
                       National Chiao Tung University - Taiwan<br/>
                       Dept. of Computer Science<br/>
                       Academics: GPA 3.9/4.3
@@ -32,7 +32,17 @@ export default class Resume extends Component {
                 </div>
               </div>
               <div className="block">
-              <div className="title">WORK EXPERIENCE</div>
+                <div className="title">WORK EXPERIENCE</div>
+                <div className="subblock">
+                  <div className="subtitle">Teaching Assistant</div>
+                  <div className="content">
+                    <p>
+                      (Feb 2019 - Jun 2019)<br/>
+                      National Chiao Tung University - Taiwan<br/>
+                      Course: Numerical Method
+                    </p>
+                  </div>
+                </div>
               </div>
               <div className="block">
               <div className="title">SKILLS</div>
