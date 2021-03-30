8d5381178da798459ed0c801708f98daa8298c5d
Yunrou <tao86419@gmail.com>
2021-03-30 16:36:27 +0800

    Update profile

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,25 +17,27 @@ export default class Home extends Component {
                 <div className="content">
                   <p>
                     Hi, I am Yun-Rou (Lilian) Lin, a computer science student, 
-                    currently studying for the master degree at National Chiao Tung University in Taiwan.
+                    currently studying for a master's degree at National Chiao Tung University in Taiwan.
                     <br/><br/>
-                    I am interested in data science--including artificial intelligence, data mining, data visualization, and so on and so forth.
-                    I enjoy acquiring new knowledge, 
-                    yet I still have a long way to go due to a wide variety of knowledge in the field of computer science.
-                    "Learning is a lifelong journey!" 
+                    After graduating from the University, 
+                    I found I am really into the field of data science, thus, taking several related courses, such as artificial intelligence, data mining, data visualization, machine learning, applied statistics, and so forth.
+                    "Learning is a lifelong journey!" I enjoy acquiring new knowledge, 
+                    yet still have a long way to go due to a wide variety of knowledge in the field of computer science and data science.
+                    <br/><br/>
                     In addition, via participating in social engagements and cooperation projects in university,
-                    I become an astute and open-minded team worker. 
+                    I became an adaptable and open-minded team player. In a team, I would like to listen to others' 
+                    suggestions, thoughts, and ideas; Also, expressing my own opinions. 
+                    I believe a frank and effective discussion could accelerate our working progress, and thereby making a better decision.
                     <br/><br/>
                     Furthermore, knowledge in aesthetics and psychology rather engages me.
-                    Reading psychological ariticles and books makes me
-                    become a more mature individual with robust mindset.
+                    Reading psychological articles and books makes me
+                    become a more mature individual with a robust mindset.
                     Appreciating artworks inspires and intrigues me to come up with creative ideas,
                     and be more sensitive to the surroundings.
                     Consequently, I am capable of conquering difficulties without fear
                     and develop innovative thoughts.
                     <br/><br/>
-                    I love challenges and the sense of accomplishment. 
-                    I usually tell myself, just take risks and go for it!
+                    Besides, I am looking forward to working as a data scientist after graduation.
                   </p>
                 </div>
               </div>
