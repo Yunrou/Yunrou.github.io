fca509dc055ee1f04fbd472c8ae5caadea31ac38
Yunrou <yunrou.cs08@nycu.edu.tw>
2022-05-27 15:40:10 +0800

    About Me

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,28 +16,20 @@ export default class Home extends Component {
                 <div className="title">ABOUT ME</div>
                 <div className="content">
                   <p>
-                    Hi, I am Yun-Rou (Lilian) Lin, a computer science student, 
-                    currently studying for a master's degree at National Chiao Tung University in Taiwan.
+                    Hi, I am LILIAN! 
                     <br/><br/>
-                    After graduating from the University, 
-                    I found I am really into the field of data science, thus, taking several related courses, such as artificial intelligence, data mining, data visualization, machine learning, applied statistics, and so forth.
-                    "Learning is a lifelong journey!" I enjoy acquiring new knowledge, 
-                    yet still have a long way to go due to a wide variety of knowledge in the field of computer science and data science.
+                    I am a programmer graduate from NYCU, TW, with a passion for data science and multimedia engineering.
+                    In graduate school, my research focused on the color encodings for multi-view visualizations (e.g., dashboards), 
+                    and I developed an interactive recommendation tool that mainly deals with the color consistency for MVs.
                     <br/><br/>
-                    In addition, via participating in social engagements and cooperation projects in university,
-                    I became an adaptable and open-minded team player. In a team, I would like to listen to others' 
-                    suggestions, thoughts, and ideas; also, expressing my own opinions. 
-                    I believe a frank and effective discussion could accelerate our working progress, and thereby making a better decision.
+                    "Learning is a lifelong journey!" I enjoy acquiring new knowledge yet still have a long way to go.
                     <br/><br/>
-                    Furthermore, knowledge in aesthetics and psychology rather engages me.
-                    Reading psychological articles and books makes me
-                    become a more mature individual with a robust mindset.
-                    Appreciating artworks inspires and intrigues me to come up with creative ideas,
-                    and be more sensitive to the surroundings.
-                    Consequently, I am capable of conquering difficulties without fear
-                    and develop innovative thoughts.
+                    In addition, I became an adaptable and open-minded team player via participating in social engagements and cooperation projects in school.
+                    In a team, I would like to listen to and organize others' 
+                    suggestions, comments, and ideas and also express my own opinions. 
                     <br/><br/>
-                    Besides, I look forward to working as a data scientist after graduation.
+                    Aside from being a software developer, I want to be a creator to create values and make contribution, like Hayao Miyazaki, 
+                    seeking inspirations, identifying the needs and limitations, and producing innovate products with clear concepts and definition.
                   </p>
                 </div>
               </div>
