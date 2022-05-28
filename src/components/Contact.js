66443b41e3ea0a1edefbe76d56f28a2990cf7871
Yunrou <yunrou.cs08@nycu.edu.tw>
2022-05-28 15:56:29 +0800

    external link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,9 +7,12 @@ export default class AboutMe extends Component {
       <Fragment>
         <div className="contact">
           <div className="side-title">Contact <i class="fa fa-envelope-o" aria-hidden="true"></i></div>
+          <div className="side-content">Email: yunrou.cs08@nycu.edu.tw</div>
           <div className="side-content">
-          Email: yunrou.cs08@nycu.edu.tw
+          <a className="external-link linkedin"><i className="fa fa-linkedin"></i></a>
+          <a className="external-link github"><i className="fa fa-github"></i></a>
           </div>
+
         </div>
       </Fragment>
     )
